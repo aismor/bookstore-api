f36a58f7f8bec8678d27eb0445d3cf7a1cc998f6
Bruno Morais <brunodemoraiss@gmail.com>
2021-09-18 19:34:16 -0300

    Impl repositorios

---
+++ src/main/java/com/bno/bookstore/domain/Livro.java
@@ -0,0 +1,31 @@
+package com.bno.bookstore.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Livro implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String titulo;
+	private String nomeAutor;
+	private String texto;
+
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+
+}
