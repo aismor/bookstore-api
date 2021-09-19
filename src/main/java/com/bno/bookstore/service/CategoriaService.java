d8a7caa8a6ae247b7a7341a66e6fdf7251fb0a05
Bruno Morais <brunodemoraiss@gmail.com>
2021-09-19 12:24:13 -0300

    Excecao personalizada pra obj nao encontrado

--- src/main/java/com/bno/bookstore/service/CategoriaService.java
+++ src/main/java/com/bno/bookstore/service/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.bno.bookstore.domain.Categoria;
 import com.bno.bookstore.repositories.CategoriaRepository;
+import com.bno.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repositopry;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repositopry.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrado Id: " + id + ", tipo: " + Categoria.class.getName()));
 	}
 }
